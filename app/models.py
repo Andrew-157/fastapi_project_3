e5aa0b290409d6a67f4726ee1a618128d02cbe87
Andrew Subotin <subotinandrey5@gmail.com>
2023-08-16 18:11:12 +0200

    UserBase schema and User model

---
+++ app/models.py
@@ -0,0 +1,7 @@
+from sqlmodel import Field
+
+from .schemas import UserBase
+
+
+class User(UserBase, table=True):
+    id: int | None = Field(primary_key=True, default=None)
---
+++ app/schemas.py
@@ -0,0 +1,9 @@
+from sqlmodel import SQLModel, Field
+from pydantic import EmailStr
+
+
+class UserBase(SQLModel):
+    username: str = Field(unique=True, index=True,
+                          max_length=255, min_length=5)
+
+    email: EmailStr = Field(unique=True, index=True)
