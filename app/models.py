750e37ddd0660753699d95955b5a0664ba8302a2
Andrew Subotin <subotinandrey5@gmail.com>
2023-08-28 15:21:10 +0200

    Question and Tag models and schemas

--- app/models.py
+++ app/models.py
@@ -1,8 +1,37 @@
-from sqlmodel import Field
+from datetime import datetime
+from sqlmodel import Field, Relationship, SQLModel
 
-from .schemas import UserBase
+from .schemas import UserBase, QuestionBase, TagBase
 
 
 class User(UserBase, table=True):
     id: int | None = Field(primary_key=True, default=None)
     hashed_password: str
+
+    questions: list["Question"] = Relationship(
+        back_populates="user", sa_relationship_kwargs={"cascade": "delete"})
+
+
+class TaggedQuestions(SQLModel, table=True):
+    __tablename__ = 'tagged_questions'
+    recommendation_id: int | None = Field(
+        foreign_key='question.id', primary_key=True, default=None
+    )
+    tag_id: int | None = Field(
+        foreign_key='tag.id', primary_key=True, default=None
+    )
+
+
+class Question(QuestionBase, table=True):
+    id: int | None = Field(primary_key=True, default=None)
+    slug: str = Field(min_length=5, max_length=300)
+    published: datetime = Field(default=datetime.utcnow())
+    updated: datetime | None = Field(default=None)
+    user_id: int = Field(foreign_key="user.id")
+
+    user: User = Relationship(back_populates='questions')
+    tags: list["Tag"] = Relationship(link_model=TaggedQuestions)
+
+
+class Tag(TagBase, table=True):
+    id: int | None = Field(primary_key=True, default=None)
--- app/schemas.py
+++ app/schemas.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from sqlmodel import SQLModel, Field
 from pydantic import EmailStr, BaseModel
 
@@ -25,3 +26,34 @@ class UserUpdate(SQLModel):
     username: str | None = Field(max_length=255, min_length=5,
                                  default=None)
     email: EmailStr | None = Field(default=None)
+
+
+class QuestionBase(SQLModel):
+    title: str = Field(max_length=255, min_length=5)
+    content: str
+
+
+class QuestionCreate(QuestionBase):
+    tags: list[str] = Field(min_items=1)
+
+
+class QuestionRead(QuestionBase):
+    id: int
+    published: datetime
+    updated: datetime | None
+    user: UserRead
+    tags: list["TagRead"]
+
+
+class QuestionUpdate(SQLModel):
+    title: str | None = Field(min_length=5, max_length=255)
+    content: str | None = Field(default=None)
+    tags: list[str] | None = Field(default=None)
+
+
+class TagBase(SQLModel):
+    name: str = Field(max_length=255, min_length=5)
+
+
+class TagRead(TagBase):
+    id: int
